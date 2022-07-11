081bf3fb1181d9fe7c5895433008d2be457ed87d
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-07-11 20:50:55 +0530

    Update Index.js

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+import PostPreview from "./PostPreview";
 
 
 /*
@@ -14,12 +16,65 @@ import React from 'react';
     Your code should not assume that number of posts will be 100, can be 20 or 30 but always in mutiples of 10. <br/>
     So show only required number of buttons
 */
+const Buttons = ({ pageHandler, length }) => {
+  let count = length / 10;
+  if (length % 10 > 0) count += 1;
+  let arr = [];
+  for (let i = 1; i <= count; i++) {
+    arr.push(i);
+  }
+  return arr.map((button) => (
+    <button onClick={() => pageHandler(button)} id={`page-${button}`}>
+      {button}
+    </button>
+  ));
+};
 
 export const Index = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [allData, setAllData] = useState(null);
+  const [data, setData] = useState(null);
 
-    return( 
-        <div id="index">
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((response) => response.json())
+      .then((json) => {
+        setAllData(json);
+        setIsLoading(false);
+        manageData();
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-        </div>
-    )
-}
+  const manageData = () => {
+    if (allData == null) return;
+    let realPage = page * 10;
+    let tmp = allData.filter((e) => e.id > realPage - 10 && e.id <= realPage);
+    setData(tmp);
+  };
+
+  useEffect(() => {
+    manageData();
+  }, [page, allData]);
+
+  const pageHandler = (buttonId) => {
+    setPage(buttonId);
+  };
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div id="index">
+      <ul id="postsList">
+        {data.map((e, id) => (
+          <li key={e.id}>
+            <PostPreview element={e} classId={id} />
+          </li>
+        ))}
+      </ul>
+      <Buttons pageHandler={pageHandler} length={allData.length} />
+    </div>
+  );
+};
